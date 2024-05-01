a6df7064abbf4c2c41d999e9031133874096855b
Maria <ev.maria.pro@gmail.com>
2024-05-01 15:49:14 -0400

    Add recent changes

--- server/api.js
+++ server/api.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const minionsWorkRouter = require('./minions/:minionId/work');
 
+apiRouter.use('./minions/:minionId/work', minionsWorkRouter)
 
 module.exports = apiRouter;
