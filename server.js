53baf5c0c517200ca32002c51d5be5b18ec30396
Maria <ev.maria.pro@gmail.com>
2024-06-23 12:29:19 -0400

    Chore: code refactoring

--- server.js
+++ server.js
@@ -4,18 +4,6 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
 
-module.exports = app;
-
-const { createMeeting,
-  getAllFromDatabase,
-  getFromDatabaseById,
-  addToDatabase,
-  updateInstanceInDatabase,
-  deleteFromDatabasebyId,
-  deleteAllFromDatabase } = require('./server/db.js');
-
-const checkMillionDollarIdea = require('./server/checkMillionDollarIdea.js');
-
 const PORT = process.env.PORT || 4001;
 
 // Add middleware for handling CORS requests from index.html
@@ -33,89 +21,6 @@ app.use((req, res, next) => {
 const apiRouter = require('./server/api');
 app.use('/api', apiRouter);
 
-const sendAllFromDatabase = (req, res, next) => {
-  const dataType = req.path.split('/')[1];
-  res.send(getAllFromDatabase(dataType));
-};
-
-//minions
-apiRouter.get('/minions', sendAllFromDatabase);
-apiRouter.post('/minions', (req, res, next) => {
-  res.status(201).send(addToDatabase('minions', req.body));
-});
-apiRouter.get('/minions/:minionId', (req, res, next) => {
-  const foundMinion = getFromDatabaseById('minions', req.params.minionId);
-  if (foundMinion) {
-    res.send(foundMinion);
-  } else {
-    res.status(404).send();
-  }
-});
-apiRouter.put('/minions/:minionId', (req, res, next) => {
-  const updatedMinion = updateInstanceInDatabase('minions', req.body);
-  if (updatedMinion === null) {
-    res.status(404).send();
-  } else {
-    res.send(updatedMinion);
-  }
-});
-apiRouter.delete('/minions/:minionId', (req, res, next) => {
-  const deletedMinion = deleteFromDatabasebyId('minions', req.params.minionId);
-  if (deletedMinion === true) {
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
-
-//ideas
-apiRouter.param('ideaId', (req, res, next, id) => {
-  const idea = getFromDatabaseById('ideas', id);
-  if (idea) {
-    req.idea = idea;
-    next();
-  } else {
-    res.status(404).send();
-  }
-});
-apiRouter.get('/ideas', sendAllFromDatabase);
-apiRouter.get('/ideas/:ideaId', (req, res, next) => {
-  const foundIdea = getFromDatabaseById('ideas', req.params.ideaId);
-  if (foundIdea) {
-    res.send(foundIdea);
-  } else {
-    res.status(404).send();
-  }
-});
-apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
-  const deletedIdea = deleteFromDatabasebyId('ideas', req.params.ideaId);
-  if (deletedIdea === true) {
-    res.status(204).send();
-  } else {
-    res.status(404).send();
-  }
-});
-
-apiRouter.post('/ideas', checkMillionDollarIdea, (req, res, next) => {
-  const newIdea = addToDatabase('ideas', req.body);
-  res.status(201).send(newIdea);
-});
-apiRouter.put('/ideas/:ideaId', checkMillionDollarIdea, (req, res, next) => {
-  const updatedIdea = updateInstanceInDatabase('ideas', req.body);
-  res.send(updatedIdea);
-});
-
-
-//meetings
-apiRouter.get('/meetings', sendAllFromDatabase);
-apiRouter.post('/meetings', (req, res, next) => {
-  let newMeeting = addToDatabase('meetings', createMeeting());
-  res.status(201).send(newMeeting);
-});
-apiRouter.delete('/meetings', (req, res, next) => {
-  res.status(204).send(deleteAllFromDatabase('meetings'));
-});
-
 // This conditional is here for testing purposes:
 if (!module.parent) { 
   // Add your code to start the server listening at PORT below:
@@ -123,3 +28,5 @@ if (!module.parent) {
     console.log(`Server is listening on port ${PORT}`);
   });
 };
+
+module.exports = app;
--- server/api.js
+++ server/api.js
@@ -1,11 +1,102 @@
 const express = require('express');
 const apiRouter = express.Router();
 
+const { createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase } = require('./db.js');
+
+const checkMillionDollarIdea = require('./checkMillionDollarIdea.js');
+
 const minionsWorkRouter = require('./minionWork');
 
 apiRouter.use('/minions', minionsWorkRouter);
 
+const sendAllFromDatabase = (req, res, next) => {
+    const dataType = req.path.split('/')[1];
+    res.send(getAllFromDatabase(dataType));
+};
+  
+//minions
+apiRouter.get('/minions', sendAllFromDatabase);
+apiRouter.post('/minions', (req, res, next) => {
+res.status(201).send(addToDatabase('minions', req.body));
+});
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+const foundMinion = getFromDatabaseById('minions', req.params.minionId);
+if (foundMinion) {
+    res.send(foundMinion);
+} else {
+    res.status(404).send();
+}
+});
+apiRouter.put('/minions/:minionId', (req, res, next) => {
+const updatedMinion = updateInstanceInDatabase('minions', req.body);
+if (updatedMinion === null) {
+    res.status(404).send();
+} else {
+    res.send(updatedMinion);
+}
+});
+apiRouter.delete('/minions/:minionId', (req, res, next) => {
+const deletedMinion = deleteFromDatabasebyId('minions', req.params.minionId);
+if (deletedMinion === true) {
+    res.status(204).send();
+} else {
+    res.status(404).send();
+}
+});
 
+//ideas
+apiRouter.param('ideaId', (req, res, next, id) => {
+const idea = getFromDatabaseById('ideas', id);
+if (idea) {
+    req.idea = idea;
+    next();
+} else {
+    res.status(404).send();
+}
+});
+apiRouter.get('/ideas', sendAllFromDatabase);
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+const foundIdea = getFromDatabaseById('ideas', req.params.ideaId);
+if (foundIdea) {
+    res.send(foundIdea);
+} else {
+    res.status(404).send();
+}
+});
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+const deletedIdea = deleteFromDatabasebyId('ideas', req.params.ideaId);
+if (deletedIdea === true) {
+    res.status(204).send();
+} else {
+    res.status(404).send();
+}
+});
+
+apiRouter.post('/ideas', checkMillionDollarIdea, (req, res, next) => {
+const newIdea = addToDatabase('ideas', req.body);
+res.status(201).send(newIdea);
+});
+apiRouter.put('/ideas/:ideaId', checkMillionDollarIdea, (req, res, next) => {
+const updatedIdea = updateInstanceInDatabase('ideas', req.body);
+res.send(updatedIdea);
+});
+
+
+//meetings
+apiRouter.get('/meetings', sendAllFromDatabase);
+apiRouter.post('/meetings', (req, res, next) => {
+let newMeeting = addToDatabase('meetings', createMeeting());
+res.status(201).send(newMeeting);
+});
+apiRouter.delete('/meetings', (req, res, next) => {
+res.status(204).send(deleteAllFromDatabase('meetings'));
+});
 
 
 module.exports = apiRouter;
